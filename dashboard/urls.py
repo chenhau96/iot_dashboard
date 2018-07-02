0ed3f3aa8d63d0f3ac495de974bb4eb303bcee94
chenhau96 <chenhau96@hotmail.com>
2018-07-02 22:08:25 +0800

    some ui changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -10,4 +10,13 @@ urlpatterns = [
     # path('<string:chart_name>/', views.detail, name='detail')
     # Device Management Page
     # path('devices/', views.devices, name='devices'),
+
+    # For temporary use
+    path('max_vs_min_view/', views.max_vs_min_view, name='max_vs_min_view'),
+    path('avg_temp/', views.avg_temp, name='avg_temp'),
+    path('snowView/', views.snowView, name='snowView'),
+    path('precipitationView/', views.precipitationView, name='precipitationView'),
+    path('mapView/', views.mapView, name='mapView'),
+    path('device_table/', views.device_table, name='device_table'),
+
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -9,15 +9,34 @@ for device in Devices.objects.all():
 
 def index(request):
     # TODO: index page
-    pass
-    # return render(request, 'dashboard/index.html')
+    # pass
+    return render(request, 'dashboard/index.html')
 
 def detail(request, chart_name):
     # TODO: link to a single chart page when click on the chart name
-    # return render(request, 'dashboard/detail.html')
+    # return render(request, 'dashboard/.html')
     pass
 
 def devices(request):
     # TODO: navigate to devices page
     pass
 
+
+# For temporary use
+def max_vs_min_view(request):
+    return render(request, 'dashboard/max-vs-min-view.html')
+
+def avg_temp(request):
+    return render(request, 'dashboard/avg-temp.html')
+
+def snowView(request):
+    return render(request, 'dashboard/snowView.html')
+
+def precipitationView(request):
+    return render(request, 'dashboard/precipitationView.html')
+
+def mapView(request):
+    return render(request, 'dashboard/mapView.html')
+
+def device_table(request):
+    return render(request, 'dashboard/tables.html')
