d547dee4f35d59e64eb1976b2db23e0e1664f906
chenhau96 <chenhau96@hotmail.com>
2018-07-04 21:29:51 +0800

    Added REST API call to query data from database

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,24 +1,31 @@
 from mongoengine import *
-
-
-"""
-The following models are the collections in the MongoDB
-"""
+from rest_framework_mongoengine import serializers, viewsets
 
 
 class Devices(Document):
     """
-    Devices Document/Collection
+    Devices Collection
     It contains attirbutes:
     - device_id: The id of the device
     - created_on: The datetime of the data generated, where datetime is converted to Epoch time
     - data: The data generated
     """
     device_id = StringField(max_length=30, required=True)
-    created_on = DecimalField()     # In Epoch time
+    created_on = DynamicField()     # In Epoch time
     data = DynamicField()
 
 
+class DevicesSerializer(serializers.DocumentSerializer):
+    class Meta:
+        model = Devices
+        fields = '__all__'
 
+
+class DevicesViewSet(viewsets.ModelViewSet):
+    lookup_field = 'device_id'
+    serializer_class = DevicesSerializer
+
+    def get_queryset(self):
+        return Devices.objects.all()
 
 
