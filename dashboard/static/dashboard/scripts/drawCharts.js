06496276dc5a323fe1b8abea04c434dbe3c2396e
chenhau96 <chenhau96@hotmail.com>
2018-07-09 13:36:40 +0800

    complete change color function

--- dashboard/static/dashboard/scripts/drawCharts.js
+++ dashboard/static/dashboard/scripts/drawCharts.js
@@ -1,9 +1,10 @@
   //var api = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-31&end=2018-04-01';
-  var color = "blue";
+  var color;
   var parsedData;
 
   var api = 'http://localhost:8000/api/devices';
   document.addEventListener("DOMContentLoaded", function(event) {
+    color = document.getElementById("colors").value;
    fetch(api)
      .then(function(response) { return response.json(); })
      .then(function(data) {
@@ -19,7 +20,7 @@
     for (var i in data) {
         arr.push(
            {
-               //ts: data[i].timestamp,
+               //ts: moment(data[i].timestamp).format("DD-MM-YYYY HH:MM:SS"),
                ts: i,
                temp: data[i].data["temperature"],
            }
@@ -53,10 +54,10 @@
 
   function drawChart(data) {
     //Defining time format
-     var utcParser = d3.utcParse("%Y-%m-%dT%H:%M:%S.%LZ");
+     var parseDate = d3.timeFormat("%d-%m-%Y %H:%M:%S");
 
      var svgWidth = 600, svgHeight = 380;
-     var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+     var margin = { top: 20, right: 20, bottom: 40, left: 50 };
      var width = svgWidth - margin.left - margin.right;
      var height = svgHeight - margin.top - margin.bottom;
 
@@ -72,6 +73,11 @@
      var x = d3.scaleTime().rangeRound([0, width]);
      var y = d3.scaleLinear().rangeRound([height, 0]);
 
+    var xAxis = d3.axisBottom(x)
+        .tickFormat(d3.timeFormat("%H:%M:%S"));
+
+    var yAxis = d3.axisLeft(y);
+
      var line = d3.line()
          .x(function(d) { return x(d.ts)})
          .y(function(d) { return y(d.temp)})
@@ -80,12 +86,17 @@
 
      g.append("g")
          .attr("transform", "translate(0," + height + ")")
-         .call(d3.axisBottom(x))
+         .call(xAxis)
+         .append("text")
+         .attr("fill", "#000")
+         .attr("transform", "translate(" + (width / 2) + ", " + 30 + ")")
+         .attr("text-anchor", "middle")
+         .text("Time")
          .select(".domain")
          .remove();
 
      g.append("g")
-         .call(d3.axisLeft(y))
+         .call(yAxis)
          .append("text")
          .attr("fill", "#000")
          .attr("transform", "rotate(-90)")
@@ -98,6 +109,7 @@
         .datum(data)
         .attr("fill", "none")
         .attr("stroke", color)
+        .attr("class", "data-point")
         .attr("stroke-linejoin", "round")
         .attr("stroke-linecap", "round")
         .attr("stroke-width", 1.5)
@@ -105,7 +117,6 @@
   }
 
   function changeColor(e) {
-    console.log(color);
     color = e.target.value;
-    drawChart(parsedData);
+    d3.select(".data-point").attr("stroke", color);
   }
